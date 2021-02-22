2305d4fac4f7402d7d713f39b6dc10d16f85f2d6
Qassim Alkassir <qassim.ali51@gmail.com>
2021-02-21 19:08:31 -0500

    first commit

---
+++ src/main/java/com/AQ/VaxTrack/VaxTracker/VaxTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.AQ.VaxTrack.VaxTracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VaxTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VaxTrackerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/AQ/VaxTrack/VaxTracker/VaxTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.AQ.VaxTrack.VaxTracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VaxTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
